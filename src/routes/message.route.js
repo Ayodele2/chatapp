546f211d50de8aa7e69a51aed5813f46385ca6de
Kabiyesi <ayodeleson28@gmail.com>
2025-12-18 15:03:49 +0100

    signup complete

--- src/routes/message.route.js
+++ src/routes/message.route.js
@@ -2,7 +2,14 @@ import express from 'express';
 
 const router = express.Router();
 
+
 router.get("/send", (req, res) => {
-    res.send("send message endpoint");
+    res.send("send endpoint");
 })
+
+router.get("/receive", (req, res) => {
+    res.send("receive endpoint");
+})
+
+
 export default router;
--- src/server.js
+++ src/server.js
@@ -1,18 +1,34 @@
-import express from 'express';
-import dotenv from 'dotenv';
+import express from "express"
+import dotenv from "dotenv"
+import authRoutes from "./routes/auth.route.js"
+import messageRoutes from "./routes/message.route.js"
+import path from "path"
+import { connectDB } from "./lib/db.js"
 
+// Load environment variables
+dotenv.config()
 
-import authRoutes from './routes/auth.route.js'
-import messageRoutes from './routes/message.route.js'
+const app = express()
+const __dirname = path.resolve()
+const PORT = process.env.PORT || 3000
 
-const app = express();
+// Middleware to parse JSON, req.body
+app.use(express.json())  
 
-
-dotenv.config();
-
-const PORT = process.env.PORT || 3000;
-
+// Routes
 app.use("/api/auth", authRoutes)
 app.use("/api/messages", messageRoutes)
 
-app.listen(PORT, () => console.log ("server is running on port: " + PORT))
+// Make ready for deployment 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")))
+
+  app.use((req, res) => {
+    res.sendFile(path.resolve(__dirname, "../frontend", "dist", "index.html"))
+  })
+}
+
+app.listen(PORT, () => {
+  console.log("Server is running on port: " + PORT)
+  connectDB()
+})
